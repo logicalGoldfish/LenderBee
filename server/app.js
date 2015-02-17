f3b7edd60a8e20ab43844562e6ef4b52870e0c7c
Christine Lee <cylee4@gmail.com>
2015-02-16 19:22:03 -0800

    Create database schema with sample data

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var router = require('./config.js');
 var logger = require('morgan');
-
+var db = require('./db/db.js').db;
+db();
 
 app.set('port', process.env.PORT || 3000);
 
 require('./config.js')(app, express);
+
 
 // app.get('/', function(req, res){
 //   res.render('../client/index.html');
