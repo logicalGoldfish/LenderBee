922416930a2aacfe8693b6f2c98f8b9b5a23955a
Christine Lee <cylee4@gmail.com>
2015-02-24 16:03:29 -0800

    Closes #34, getByUser

--- server/notifications/controller.js
+++ server/notifications/controller.js
@@ -6,9 +6,11 @@ var Notification = global.db.Notification;
 var controller = {};
 
 controller.create = function(req, res, next){
-	//get the item title out and the borrower name
+	//get the item title out and the borrower name from path
 	var item = req.params.item;
+	// title of item
 	var borrower = req.params.borrower;
+	// username of item
 	User.find({
 		where: {
 			username: borrower
@@ -26,12 +28,15 @@ controller.create = function(req, res, next){
 				.then(function(notification){
 					res.send(notification);
 				})
+		}).catch(function(err) {
+			console.log('\nERROR WRITING NOTIFICATION:\n', err);
 		})
 	})
 }
 
 controller.getByUser = function(req, res, next){
 	var user = req.params.user;
+	var results = [];
 	User.find({
 		where: {
 			username: user
@@ -42,19 +47,57 @@ controller.getByUser = function(req, res, next){
 			where: {
 				lender_id: user.id
 			}
+		}).catch(function(err) {
+			console.log('\nItem findAll err:\n', err);
 		}).then(function(items){
 			var itemsId = [];
-			for(var i = 0; i < items.length; i++){
+			for (var i = 0; i < items.length; i++) {
 				itemsId.push(items[i].id);
 			}
 			Notification.findAll({
 				itemreq_id: itemsId
+				// attributes: ['itemreq_id'],
+				// joinTableAttributes: ['itemreq_id']
+				// include: [{ model: Item, foreignKey: 'itemreq_id'}]
+			}).catch(function(err) {
+				console.log('\nNotifications findAll err:\n', err);
 			}).then(function(notifications){
-				res.json(notifications);
+				// change this so that response has username info and item info
+				// borrower's username, requested item's title
+				var thisMustCompleteBeforeResponse = function() {
+					for (var i = 0; i<notifications.length; i++) {
+						(function(i) {
+							var notification = notifications[i];
+							User.find({ 
+								where: {id: notification.userreq_id} 
+							}).then(function(borrower) {
+						  	notification.dataValues['userreq_username'] = borrower.username;
+						  }).then(function() {
+								Item.find({ 
+									where: {id: notification.itemreq_id} 
+								}).then(function(item) {
+							  	notification.dataValues['itemreq_title'] = item.title;
+							  	results.push(notification.dataValues);
+							  }).catch(function(err) {
+							  	console.log('\nnotifications getByUser error:', err);
+							  })
+							  .then(function() {
+							  	if (notifications.length === results.length) {
+							  		console.log('\n\nSENDING RESPONSE!\n\n')
+							  		res.json(results);
+							  	}
+							  })
+						  })
+						})(i);	
+					}
+				}
+
+				thisMustCompleteBeforeResponse();
+
 			})
 		})
 	})
 }
-
+ 
 
 module.exports = controller;
