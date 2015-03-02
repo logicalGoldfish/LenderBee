6ea5384c3aa1e13c5f7ebc53bdd7d32b289c84d1
matthewpolland <matthewpolland@gmail.com>
2015-03-01 22:05:01 -0800

    (bug) disconnect socket.io after use

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -48,7 +48,6 @@ $(document).ready(function(){
 
 		componentDidMount: function(){
 			var socket = io.connect();
-			console.log("SOCKET: ", socket);
 			socket.on("userLoad", function(data){
 				actions.mountUser(data);
 				socket.disconnect(); 
