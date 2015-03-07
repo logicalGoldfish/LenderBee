d87cf260669fadf74bc7c3ba60f93ba55e94352e
matthewpolland <matthewpolland@gmail.com>
2015-03-07 15:21:37 -0800

    (style) completed styling for item submit page

--- client/src/components/postPage.react.jsx
+++ client/src/components/postPage.react.jsx
@@ -41,29 +41,36 @@ var PostPage = React.createClass({
    */
   render: function() {
     return (
+      //later: plug in " + session.username + "
       <div>
-      <SuccessMessage message="Your Item is Posted!  " />
+      <div className="ui center aligned segment"><h2 className="ui horizontal header divider">Post an Item</h2></div>
       <form className="postForm" onSubmit={this.handlePostSubmit}>
-        <div className="form-group">
-          <label for="itemName">Enter Item</label>
-          <input type="text" className="form-control" id="itemPostTitle" placeholder="Enter Item Name (ie 'Hammer')" name="title" />
+      <div className="ui fluid form yellow segment">
+        <div className="two fields">
+          <div className="field">
+            <label for="itemName">Enter Item</label>
+            <input type="text" className="form-control" id="itemPostTitle" placeholder="Enter Item Name (ie 'Hammer')" name="title" />
+          </div>
+          <div className="field">
+            <label for="exampleInputEmail1">Enter BeeBucks</label>
+            <input type="number" min="1" step="1" className="form-control" id="itemPostBeeBucks" placeholder="$$$" name="beebucks" />
+          </div>
         </div>
-        <div className="form-group">
-        <label for="itemName">Enter Description</label>
-        <textarea className="form-control" id="itemPostDescription" rows="3" placeholder="Describe Your Item" name="description"></textarea>
+        <div className="one field">
+          <label for="itemName">Enter Description</label>
+          <textarea className="form-control" id="itemPostDescription" rows="3" placeholder="Describe Your Item" name="description"></textarea>
         </div>
-        <div className="form-group">
-          <label for="exampleInputEmail1">Enter BeeBucks</label>
-          <input type="number" min="1" step="1" className="form-control" id="itemPostBeeBucks" placeholder="$$$" name="beebucks" />
-        </div>
-        <div className="form-group">
+        <div className="one field">
           <label for="exampleInputFile">Add Photos</label>
-          <input type="file" id="itemPostPhotos" name="photos" />
-          <p className="help-block">Upload Item Photos</p>
+          <div className="ui yellow labeled icon button">Choose a file <i className="file image outline icon"></i></div>
         </div>
-        <button type="submit" class="btn btn-warning" onClick={this.handlePostSubmit}>Submit</button>
-        </form>
+        <div className="ui vertical segment">
+          <p></p>
         </div>
+        <div className="putMiddle"><button type="submit" className="massive ui green basic button" onClick={this.handlePostSubmit}>Submit</button></div>
+      </div>
+      </form>
+      </div>
     );
   },
 
